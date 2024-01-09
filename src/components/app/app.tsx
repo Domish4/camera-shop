0b997e7520d6591e1508a38c33bd1c4e98c0afa2
Alexandra <dom.levchenko.l7@gmail.com>
2024-01-09 16:54:13 +0900

    add: scroll smooth

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,12 +1,20 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import CatalogMain from '../../pages/catalog-main/catalog-main';
 import ProductCardPage from '../../pages/product-page/product-page';
 import ErrorPage from '../../pages/error-page/error-page';
 import ShoppingCart from '../../pages/shopping-cart/shopping-cart';
 import { HelmetProvider } from 'react-helmet-async';
 import { AppRoute } from '../../utils/const';
+import { useEffect } from 'react';
 
 function App(): JSX.Element {
+
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }, [location]);
+
   return (
     <HelmetProvider>
       <Routes>
